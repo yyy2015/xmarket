7a3377b7798509e2dc0c2f892ddbbd6863c10d33
yyy2015 <2456700983@qq.com>
2017-03-02 15:47:38 +0800

    one more task

--- xmarket-android/app/src/main/java/me/jcala/xmarket/AppConf.java
+++ xmarket-android/app/src/main/java/me/jcala/xmarket/AppConf.java
@@ -2,7 +2,7 @@ package me.jcala.xmarket;
 
 public interface AppConf {
 
-    boolean useMock=false;//是否使用本地离线数据，不与服务器进行交互；true表示使用离线，false使用服务器数据
+    boolean useMock=true;//是否使用本地离线数据，不与服务器进行交互；true表示使用离线，false使用服务器数据
 
     boolean enabled_ssl=true;//是否使用https协议(包括Fresco加载的图片)
 
